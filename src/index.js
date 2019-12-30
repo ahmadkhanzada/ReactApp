e3c0c1ab7f83ab3e8cbc251296cd986cd9de1f10
Ahmad khanzada <ahmadkhanzada@gmail.com>
2019-12-30 10:22:21 +0500

    learned git

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+function myFun()
+{
+    return (
+        <ul>
+            <li>poder</li>
+            <li>is</li>
+            <li>a</li>
+            <li>bitch</li>
+
+        </ul>
+
+    )
+}
+
+ReactDOM.render(<myFun/> , document.getElementById('root'));
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
